54a0af2dcd91f9409dcd6bf84d5dd5eb42244b0f
oskarleonard <oskar.leonard@hotmail.com>
2021-01-08 16:40:52 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { headerHigh } from '../api/mock/article/components/headerHigh';
+import HeaderHigh from './components/article/headerHigh/HeaderHigh';
 import "./styles.css";
 
 export default function App() {
@@ -6,6 +8,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+        <HeaderHigh headerHigh={headerHigh} />
     </div>
   );
 }
---
+++ src/components/article/headerHigh/HeaderHigh.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+function HeaderHigh({className, headerHigh}) {
+    return (
+        <div className={classNames('test')}>
+            <h3>{headerHigh?.title}</h3>
+        </div>
+    );
+}
+
+HeaderHigh.propTypes = {
+    className: PropTypes.string,
+    headerHigh: PropTypes.shape({
+        to: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    })
+};
