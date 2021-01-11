c9b18f10592dcecec25263fe6b12613704f845fa
oskarleonard <oskar.leonard@hotmail.com>
2021-01-11 10:24:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { headerHigh } from './api/mock/article/components/headerHigh';
-import HeaderHigh from './components/article/headerHigh/HeaderHigh';
+import { headerHigh } from "./api/mock/article/components/headerHigh";
+import HeaderHigh from "./components/article/headerHigh/HeaderHigh";
 import "./styles.css";
 
 export default function App() {
@@ -8,8 +8,13 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-        <HeaderHigh headerHigh={headerHigh} />
-        <HeaderHigh headerHigh={headerHigh} />
+      <HeaderHigh headerHigh={headerHigh} />
+      <HeaderHigh headerHigh={headerHigh} />
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
     </div>
   );
 }
